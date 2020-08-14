44388f83fcecaffd7beead565fc86346d84dbc94
Matthew Joseph <matthew.j.joe@gmail.com>
2020-08-14 13:33:05 -0400

    Add files via upload
    Fixed issue with not being able to see all images uploaded until the user uploaded first.

--- upload/index.php
+++ upload/index.php
@@ -1,19 +1,7 @@
 <?php 
-session_start();
-
-include($_SERVER['DOCUMENT_ROOT']. '/blm/include/config.php');
-$_COOKIE['varname'] = 42;
-
-
-//variable that can sees all the items in the 'upload folder'
-$imgDir = $_SERVER['DOCUMENT_ROOT']. '/blm/uploads/';
-$uploadedImages = scandir($imgDir);
-
-$_SESSION["rawImgArr"] = $uploadedImages;
-
+include($_SERVER['DOCUMENT_ROOT']. '/include/config.php');
 
 if (isset($_POST['upload'])) {
-print_r($uploadedImages);
 
 //Get image name
 $image = $_FILES['image']['name'];
@@ -22,17 +10,17 @@ $image = $_FILES['image']['name'];
 $state = $_POST['state_entered'];
 
 //image file directory plus uploaded image name
-$target =  $_SERVER['DOCUMENT_ROOT'] . "/blm/uploads/" . basename($image);
+$target =  $_SERVER['DOCUMENT_ROOT'] . "/uploads/" . basename($image);
 
 //isert image into mySQL table for "dba_blm"
 $sql = "INSERT INTO images (image, state) VALUES ('$image', '$state')"; 
 mysqli_query($db, $sql);
 
 if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-    $msg = "Image uploaded successfully";
+    $msg = "Image uploaded successfully!";
 
 }else{
-    $msg = "Failed to upload image";
+    $msg = "Failed to upload image.";
 }
 }
 $result = mysqli_query($db, "SELECT * FROM images");
@@ -58,7 +46,7 @@ $result = mysqli_query($db, "SELECT * FROM images");
                 <input type="file" name="image" id="file">
                 <span class="file-custom"></span>
             </label>   
-            <input style="text-align:center; display:block; margin:0 auto; margin-top:20px;" type="submit" name="upload" value="Upload Image" >
+            <input id="submitUpload" type="submit" name="upload" value="UPLOAD" >
             <div style='display:block; text-align:center; color:white; margin-top:10px;'> <?php echo $msg ?> </div>
         </form>
 
@@ -66,7 +54,6 @@ $result = mysqli_query($db, "SELECT * FROM images");
             // while ($row = mysqli_fetch_array($result)) {
             // echo "<div>";
             //     echo "<img id='img_div' src='" . $_SERVER['DOCUMENT_ROOT'] . '/blm/uploads/' . $row['image']."' >";   
-            //     echo "<div>" . $row['state'] . "</div>";
             // echo "</div>";
             // }
         ?>
